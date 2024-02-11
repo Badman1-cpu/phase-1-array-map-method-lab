082b120d3fcc44702cb3d3c9c81ac185187ce914
Tayler <taylerblevins@gmail.com>
2024-02-11 14:02:17 -0500

    done

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCase(str) {
+  // Split the string into words
+  let words = str.split(' ');
+
+  // Capitalize the first letter of each word
+  let capitalizedWords = words.map(word => {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  });
+
+  // Join the words back together with spaces
+  return capitalizedWords.join(' ');
 }
+
+function titleCased() {
+  // Map over each tutorial and convert it to title case
+  return tutorials.map(titleCase);
+}
+
+const titledTutorials = titleCased();
+console.log(titledTutorials);
